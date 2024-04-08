cad99b13d3565d0a40ea50e656e90d7dc164b0b4
Fabricio Vianna <fabriciohenriquemviana@gmail.com>
2024-04-07 22:57:56 -0300

    feat: adding dashboard main file

---
+++ dashboards.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+
+st.set_page_config(layout="wide")
+df = pd.read_csv("teste.csv", sep=";", decimal=",")
+df["Date"] = pd.to_datetime(df["Date"])
+df = df.sort_values("Date")
+
+df["Month"] = df["Date"].apply(lambda x: str(x.year) + "-" + str(x.month))
+month= st.sidebar.selectbox("Mês", df["Month"].unique())
+
+df_filtered = df[df["Month"] == month]
+
+col1, col2 = st.columns(2)
+col3, col4, col5 = st.columns(3)
+
+fig_date = px.bar(df_filtered, x="Date", y="Total", color="City", title="Faturamento por dia")
+col1.plotly_chart(fig_date)
